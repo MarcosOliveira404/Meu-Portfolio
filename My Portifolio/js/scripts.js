d48c90e7508a1b62c6b845c194afed04e122e6f1
Marcos Rikelme <120138771+MarcosOliveira404@users.noreply.github.com>
2024-06-16 16:08:01 -0300

    ScriptJS

--- My Portifolio/js/scripts.js
+++ My Portifolio/js/scripts.js
@@ -1,2 +1,14 @@
 
 //window.alert('O site ainda está passando \n por alterações ');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const floatElement = document.querySelector('.float-element');
+
+    floatElement.addEventListener('click', function() {
+        if(floatElement.computedStyleMap.animationPlayState === 'paused'){
+            floatElement.computedStyleMap.animationPlayState= 'running';
+        } else {
+            floatElement.computedStyleMap.animationPlayState = 'paused';
+        }
+    });
+});
